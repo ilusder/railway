db5f6f166f65ec18459a60c1027f841b3c4a39a8
ilus <ilusder@gmail.com>
2022-04-13 22:30:21 +0300

    Run train in the railway

--- rail.py
+++ rail.py
@@ -12,7 +12,7 @@ class RTC_Time:
         return datetime.now()
 
     def get_timer_value(self):
-        return datetime.now() - self.start_time
+        return (datetime.now() - self.start_time).total_seconds()
 
 
 class Rail_Line:
@@ -24,15 +24,26 @@ class Rail_Line:
     def __init__(self, lenght) -> None:
         self.length = lenght
 
-    def start_rail(self, speed):
+    def start_rail(self, speed, timer):
         if self.busy_flag == False and self.length > 0:
             self.busy_flag = True
             self.speed_train = speed
+            self.start_time = timer.get_time()
 
-    def exit_rail(self):
+    def on_exit_rail(self, timer):
         if self.busy_flag == True:
-            self.busy_flag = False
-    
+            if self.length // self.speed_train > timer.get_timer_value():
+                self.busy_flag = False
+                self.speed_train = 0
+
+
+def main():
+    rtc_time = RTC_Time()
+    line = Rail_Line(300000)
+    line.start_rail(3, rtc_time)
+    while line.busy_flag:
+        line.on_exit_rail(rtc_time)
 
     
-
+if __name__ == '__main__':
+    main()
